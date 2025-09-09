3e149ee84ff4ebe88e67f6837a8c3b4a47bcfeab
Julius <jr1184404@swccd.edu>
2025-09-09 20:06:16 +0000

    Created Maximum.java

---
+++ Maximum.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Maximum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int number1 = input.nextInt();
+        System.out.println("Enter second number: ");
+        int number2 = input.nextInt();
+        System.out.println("Enter third number: ");
+        int number3 = input.nextInt();
+        int max;
+
+        if (number1 >= number2) {
+            max = number1;
+        }
+        else max = number2;
+        
+            if (number1 >= number3) {
+            max = number1;
+        }
+        else max = number3;
+        
+        if (number2 >= number3) {
+            max = number2;
+        }
+        else max = number3;    
+
+        System.out.println("Maximum is: " + max);
+    }
+}
