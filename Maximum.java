6e9a4975513887d408fb92107dd0a1f4c1921bea
Julius <jr1184404@swccd.edu>
2025-09-11 20:06:14 +0000

    Updated Maximum.java

--- Maximum.java
+++ Maximum.java
@@ -1,36 +1,38 @@
 
-/*@julsrent06
- * Julius Renteria and Karen Sandoval 
+/* This program takes three different numbers and prints out the maximum number from the set.
+ * @julsrent06
+ * Julius Renteria
  */
 import java.util.Scanner;
 
 public class Maximum {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        /* Keyboard Input */
+        // First keyboard input //
         System.out.println("Enter first number: ");
         int number1 = input.nextInt();
+        // Second keyboard input //
         System.out.println("Enter second number: ");
         int number2 = input.nextInt();
+        // Third keyboard input //
         System.out.println("Enter third number: ");
         int number3 = input.nextInt();
+        //Creates the max variable //
         int max;
 
+        // First if statement to check the first two numbers //
         if (number1 >= number2) {
             max = number1;
+        } else {
+            max = number2; // Initial max //
         }
-        else max = number2;
-        
-            if (number1 >= number3) {
-            max = number1;
+        // Second if statement to check the initial max and the third number //
+            if (max >= number3) {
+            System.out.println("Maximum is: " + max); // Keeps the initial max and prints it //
+        } else {
+            max = number3; // Changes the initial max //
+            System.out.println("Maximum is: " + max); // Prints the new max //
         }
-        else max = number3;
-        
-        if (number2 >= number3) {
-            max = number2;
-        }
-        else max = number3;    
 
-        System.out.println("Maximum is: " + max);
     }
 }
